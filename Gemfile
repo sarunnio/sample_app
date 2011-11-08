960fdefd135765dc6a97564805703bcce05e342a
sarunnio <sarunnio@hotmail.com>
2011-11-08 11:53:39 +0700

    added user model

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.1'
+gem 'annotate', :git => 'git://github.com/ctran/annotate_models.git'
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20111108043113) do
+
+  create_table "promotors", :force => true do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
